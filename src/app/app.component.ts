1350f3584bc8a2003c573e917178cb4ede815382
Max Kurapov <max.kurapov@rallyteam.com>
2017-05-18 12:37:53 -0700

    adding styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Entry } from './classes/Entry';
 import { EntryService } from './entry.service';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../styles/app.component.scss'],
-  providers: [EntryService]
+  providers: [EntryService],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: '0' }),
+        animate('.8s ease-out', style({ opacity: '1' })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   public entries : Entry[] = []
@@ -27,7 +41,7 @@ export class AppComponent {
 
   addNewEntry(newEntry: Entry)
   {
-   
+  
     this.entryService.addEntry(newEntry);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SingleEntryComponent } from './components/single-entry/single-entry.component';
@@ -16,7 +17,8 @@ import { InputFormComponent } from './components/input-form/input-form.component
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
